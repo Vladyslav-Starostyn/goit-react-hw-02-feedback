aefc453dc3d2ebd9deae00e5e0cb322e9d28fe3a
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2024-07-14 14:16:36 +0300

    ----

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,19 +7,17 @@ import {
 
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
-    <div>
-      <ContainerStatistics>
-        <StatisticsList>
-          <StatisticsItem>Good: {good}</StatisticsItem>
-          <StatisticsItem>Neutral: {neutral}</StatisticsItem>
-          <StatisticsItem>Bad: {bad}</StatisticsItem>
-          <StatisticsItem>Total: {total()}</StatisticsItem>
-          <StatisticsItem>
-            Positive feedback: {positivePercentage() + '%'}
-          </StatisticsItem>
-        </StatisticsList>
-      </ContainerStatistics>
-    </div>
+    <ContainerStatistics>
+      <StatisticsList>
+        <StatisticsItem>Good: {good}</StatisticsItem>
+        <StatisticsItem>Neutral: {neutral}</StatisticsItem>
+        <StatisticsItem>Bad: {bad}</StatisticsItem>
+        <StatisticsItem>Total: {total()}</StatisticsItem>
+        <StatisticsItem>
+          Positive feedback: {positivePercentage() + '%'}
+        </StatisticsItem>
+      </StatisticsList>
+    </ContainerStatistics>
   );
 }
 
